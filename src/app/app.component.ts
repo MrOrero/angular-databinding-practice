c75b5791e432ae0a095eb712b9c43618cea26f06
sfxoozore <oozore@seamfix.com>
2023-02-07 09:50:26 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'databinding-practice';
+  username = '';
+
+  isUsernameEmpty() {
+    return this.username.length ? false : true;
+  }
+
+  clearUsername() {
+    this.username = '';
+  }
 }
